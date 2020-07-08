31027e2d968a4bfc2d5ac0b4ae28612116f93f01
Henrique Riccio <henrique.riccio@gmail.com>
2020-07-08 16:59:29 -0300

    using sqlite

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM maven:3.6.3-openjdk-14-slim
+FROM maven:3.6.3-openjdk-14-slim AS build-env
 
 COPY ./pom.xml ./pom.xml
 RUN mvn dependency:go-offline -B
@@ -6,9 +6,15 @@ RUN mvn dependency:go-offline -B
 COPY ./src ./src
 RUN mvn package
 
-FROM openjdk:14-slim
-COPY --from=0 ./target/contacts-backend-kotlin-spring-0.0.1-SNAPSHOT.jar app.jar
+FROM openjdk:14-alpine
+WORKDIR /app
+
+COPY --from=build-env ./target/contacts-backend-kotlin-spring-0.0.1-SNAPSHOT.jar app.jar
 EXPOSE 8010
+
+VOLUME data
+ENV url=jdbc:sqlite:/data/contacts.db
+
 CMD ["java","-jar","app.jar"]
 
 
---
+++ src/main/kotlin/com/henriquericcio/contacts/infra/hibernate/SQLiteIdentityColumnSupport.kt
@@ -0,0 +1,21 @@
+package com.henriquericcio.contacts.infra.hibernate
+
+import org.hibernate.MappingException
+import org.hibernate.dialect.identity.IdentityColumnSupportImpl
+
+
+class SQLiteIdentityColumnSupport : IdentityColumnSupportImpl() {
+    override fun supportsIdentityColumns(): Boolean {
+        return true
+    }
+
+    @Throws(MappingException::class)
+    override fun getIdentitySelectString(table: String, column: String, type: Int): String {
+        return "select last_insert_rowid()"
+    }
+
+    @Throws(MappingException::class)
+    override fun getIdentityColumnString(type: Int): String {
+        return "integer"
+    }
+}
